f29f40b474fa2bcd363a4a259de76c1f0f3233bf
AlibekSerikbayev <maftunayigitaliyeva4@gmail.com>
2024-12-13 12:14:44 +0500

    uzgarish

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import pandas as np
 import joblib
 
 # Modelni yuklash
@@ -27,53 +26,105 @@ price_ranges = {
     'Yunusobod': (12500, 62552.72, 425600)
 }
 
-# Ma'lumotlarni yuklash va unique qiymatlarni olish
-file_path = 'uynarxi.csv'
-data = pd.read_csv(file_path)
-districts = data['district'].unique()
+districts = list(price_ranges.keys())
 
 # Foydalanuvchi kirishlari
 selected_district = st.selectbox("Tumanni tanlang", districts)
 if selected_district in price_ranges:
     min_price, mean_price, max_price = price_ranges[selected_district]
-    #st.write(f"{selected_district} uchun narxlar diapazoni: Min: {min_price}, O'rtacha: {mean_price}, Max: {max_price}")
+    st.write(f"{selected_district} uchun narxlar diapazoni: Min: {min_price}, O'rtacha: {mean_price}, Max: {max_price}")
 
-rooms = st.number_input("Xonalar soni", min_value=1, step=1)
 size = st.number_input("Maydoni (m²)", min_value=1.0, step=1.0)
-level = st.number_input("Qavat raqami", min_value=1, step=1)
-max_levels = st.number_input("Umumiy qavatlar soni", min_value=1, step=1)
 
 # Bashorat qilish tugmasi
 if st.button("Narxni bashorat qilish"):
     # Foydalanuvchi kiritgan ma'lumotlardan massiv yaratish
     user_data = {
-        'district': [selected_district],
-        'rooms': [rooms],
-        'size': [size],
-        'level': [level],
-        'max_levels': [max_levels]
+        'size': [size]
     }
 
-    # DataFrame yaratish
-    user_df = pd.DataFrame(user_data)
-
-    # Kategorik ustunni kodlash
-    user_df = pd.get_dummies(user_df, drop_first=True)
-
-    # Modellashda ishlatilgan ustunlarni tekshirish va to'ldirish
-    model_columns = pd.get_dummies(data.drop(columns=['price']), drop_first=True).columns
-    for col in model_columns:
-        if col not in user_df.columns:
-            user_df[col] = 0
-
-    # Ustunlarning tartibini moslashtirish
-    user_df = user_df[model_columns]
-
     # Bashorat qilish
-    prediction = model.predict(user_df)[0]
+    prediction = model.predict(pd.DataFrame(user_data))[0]
 
     # Natijani chiqarish
     st.write(f"Bashorat qilingan uy narxi: {prediction:.2f}")
+
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+# import joblib
+
+# # Modelni yuklash
+# model_filename = 'uynarxi_randomforest.pkl'
+# model = joblib.load(model_filename)
+
+# # Sahifa sozlamalari
+# st.title("Uy narxini bashorat qilish")
+# st.write("Iltimos, uy haqidagi ma'lumotlarni kiriting va narxini bashorat qiling.")
+
+# # Tumanga oid narx diapazonlari
+# price_ranges = {
+#     'Bektemir': (19000, 29462.5, 40000),
+#     'Chilonzor': (12000, 47606.42, 644000),
+#     'Mirobod': (10500, 97400.88, 800000),
+#     'Mirzo Ulugbek': (10500, 57081.38, 504000),
+#     'Olmzor': (15500, 52045.55, 330000),
+#     'Sergeli': (13500, 43673.18, 120000),
+#     'Shayhontohur': (21000, 69446.2, 260000),
+#     'Uchtepa': (12300, 44558.04, 126000),
+#     'Yakkasaroy': (14500, 69535.19, 420000),
+#     'Yangihayot': (23000, 37000, 61900),
+#     'Yashnobod': (10500, 50671.55, 300000),
+#     'Yunusobod': (12500, 62552.72, 425600)
+# }
+
+# # Ma'lumotlarni yuklash va unique qiymatlarni olish
+# file_path = 'uynarxi.csv'
+# data = pd.read_csv(file_path)
+# districts = data['district'].unique()
+
+# # Foydalanuvchi kirishlari
+# selected_district = st.selectbox("Tumanni tanlang", districts)
+# if selected_district in price_ranges:
+#     min_price, mean_price, max_price = price_ranges[selected_district]
+#     #st.write(f"{selected_district} uchun narxlar diapazoni: Min: {min_price}, O'rtacha: {mean_price}, Max: {max_price}")
+
+# rooms = st.number_input("Xonalar soni", min_value=1, step=1)
+# size = st.number_input("Maydoni (m²)", min_value=1.0, step=1.0)
+# level = st.number_input("Qavat raqami", min_value=1, step=1)
+# max_levels = st.number_input("Umumiy qavatlar soni", min_value=1, step=1)
+
+# # Bashorat qilish tugmasi
+# if st.button("Narxni bashorat qilish"):
+#     # Foydalanuvchi kiritgan ma'lumotlardan massiv yaratish
+#     user_data = {
+#         'district': [selected_district],
+#         'rooms': [rooms],
+#         'size': [size],
+#         'level': [level],
+#         'max_levels': [max_levels]
+#     }
+
+#     # DataFrame yaratish
+#     user_df = pd.DataFrame(user_data)
+
+#     # Kategorik ustunni kodlash
+#     user_df = pd.get_dummies(user_df, drop_first=True)
+
+#     # Modellashda ishlatilgan ustunlarni tekshirish va to'ldirish
+#     model_columns = pd.get_dummies(data.drop(columns=['price']), drop_first=True).columns
+#     for col in model_columns:
+#         if col not in user_df.columns:
+#             user_df[col] = 0
+
+#     # Ustunlarning tartibini moslashtirish
+#     user_df = user_df[model_columns]
+
+#     # Bashorat qilish
+#     prediction = model.predict(user_df)[0]
+
+#     # Natijani chiqarish
+#     st.write(f"Bashorat qilingan uy narxi: {prediction:.2f}")
 
     
 
